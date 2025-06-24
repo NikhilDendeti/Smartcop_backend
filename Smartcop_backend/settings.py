d459622087fa5cfd863c5e175cecf030adf8a690
Nikhil Dendeti <145838281+NikhilDendeti@users.noreply.github.com>
2025-06-24 11:46:53 +0530

    Fix CORS configuration

--- Smartcop_backend/settings.py
+++ Smartcop_backend/settings.py
@@ -46,15 +46,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Smartcop_backend.urls'
 
@@ -128,7 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "https://smart-complaint-pilot.vercel.app",
@@ -137,4 +137,4 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_HEADERS = ["*"]
 CORS_ALLOW_METHODS = ["*"]
-CORS_ALLOW_ALL_ORIGINS = True
+Access-Control-Allow-Origin: https://ai-cop-frontend.vercel.app
